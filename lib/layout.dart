150d1018e696111413290d94886f14148e605288
thisistreamliner <thisistreamliner@gmail.com>
2021-10-04 15:44:30 +0300

    add top navigation

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:web_dashboard/helpers/responsiveness.dart';
 import 'package:web_dashboard/widgets/large_screen.dart';
 import 'package:web_dashboard/widgets/small_screen.dart';
+import 'package:web_dashboard/widgets/top_nav_bar.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
+      appBar: topNavigationBar(
+        context,
+        scaffoldKey,
       ),
       body: ResponsiveWidget(
         largeScreen: LargeScreen(),
