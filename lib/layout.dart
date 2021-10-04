85414a326ee1ed69f879696e1eb738d7166aca98
thisistreamliner <thisistreamliner@gmail.com>
2021-10-04 15:11:53 +0300

    add responsive widget

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:web_dashboard/helpers/responsiveness.dart';
 import 'package:web_dashboard/widgets/large_screen.dart';
+import 'package:web_dashboard/widgets/small_screen.dart';
 
 class SiteLayout extends StatelessWidget {
   const SiteLayout({Key? key}) : super(key: key);
@@ -11,7 +13,10 @@ class SiteLayout extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: LargeScreen(),
+      body: ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: SmallScreen(),
+      ),
     );
   }
 }
